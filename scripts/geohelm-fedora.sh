bcba2f146774c07441c7f30d2f7196332167275a
David Ghedini <david@davidghedini.com>
2017-07-10 18:35:43 +0300

    geohelm-fedora.sh
    geohelm-fedora.sh

---
+++ scripts/geohelm-fedora.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+#GeoHelm Pre-Install Script for Fedora
+#For use on clean Fedora 24 or 25 box only!!
+
+#Usage: wget https://raw.githubusercontent.com/downloads/AcuGIS/geohelm-quick-start/geohelm-fedora.sh
+#chmod +x geohelm-fedora.sh
+#./geohelm-fedora.sh
+ 
+function install_webmin(){
+	#http://doxfer.webmin.com/Webmin/Installation
+	cat >/etc/yum.repos.d/webmin.repo <<EOF
+[Webmin]
+name=Webmin Distribution Neutral
+baseurl=http://download.webmin.com/download/yum
+enabled=1
+gpgcheck=1
+gpgkey=http://www.webmin.com/jcameron-key.asc
+EOF
+	dnf -y install webmin
+}
+ 
+function download_geohelm_module(){
+	pushd /tmp/
+ 
+	wget https://github.com/AcuGIS/GeoHelm/archive/master.zip
+	unzip master.zip
+	mv GeoHelm-master geohelm
+	tar -czf /opt/geohelm.wbm.gz geohelm
+	rm -rf geohelm master.zip
+ 
+	popd
+}
+
+function warn_apache(){
+
+ 
+	 echo -e "Webmin is now installed and GeoHelm module is at /opt/geohelm.wbm.gz"
+        echo -e "Important: Install Apache via Webmin BEFORE installing GeoHelm!!!" 
+}
+ 
+dnf -y install wget unzip bzip2
+ 
+install_webmin;
+download_geohelm_module;
+warn_apache;
