1d5f8e0a58e15171ce44ebff2915dec1dceeb8d8
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2018-08-02 16:17:26 +0300

    Remove duplicate function from PG lib

--- pg-lib.pl
+++ pg-lib.pl
@@ -68,26 +68,3 @@ sub have_pg_repo(){
 
 	return $found;
 }
-
-sub check_pg_ext_deps{
-	my $pg_ver = $_[0];
-
-	my @ext_pkgs;
-
-	my %osinfo = &detect_operating_system();
-	if( ($osinfo{'os_type'} =~ /debian/i)){
-		@ext_pkgs = ("postgresql-$pg_ver-postgis-scripts", "postgresql-$pg_ver-pgrouting-scripts", "postgresql-$pg_ver-pgrouting");
-	}elsif( $osinfo{'os_type'} =~ /redhat/i){
-		my $pg_ver2;
-		($pg_ver2 = $pg_ver) =~ s/\.//;
-		@ext_pkgs = ("postgis23_$pg_ver2", "pgrouting_$pg_ver2", "postgresql$pg_ver2-contrib");
-	}
-
-	foreach my $pkg (@ext_pkgs){
-		my @pinfo = software::package_info($pkg);
-		if(!@pinfo){
-			print "<p>Warning: $pkg package is not installed. Install it manually or ".
-				  "<a href='../software/install_pack.cgi?source=3&update=$pkg&return=%2E%2E%2Fgeohelm%2F&returndesc=GeoHelm&caller=geohelm'>click here</a> to have it downloaded and installed.</p>";
-		}
-	}
-}
