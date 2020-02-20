d6cb3572db83f5a853425eb56bd6dc03f9f7cae0
David Ghedini <david@davidghedini.com>
2020-02-20 19:11:46 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'GeoHelm'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
+  ('index', 'GeoHelm.tex', u'GeoHelm Documentation',
    u'Read the Docs', 'manual'),
 ]
 
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'GeoHelm', u'GeoHelm Documentation',
+     [u'GeoHelm'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'GeoHelm', u'GeoHelm Documentation',
+   u'GeoHelm', 'GeoHelm', 'AcuGIS GeoHelm.',
    'Miscellaneous'),
 ]
 
