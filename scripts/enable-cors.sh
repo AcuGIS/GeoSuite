a33e646f28e3a991ebb5ed83f29c353c48739b4a
David Ghedini <david@davidghedini.com>
2020-04-12 14:20:27 +0300

    Update enable-cors.sh

--- scripts/enable-cors.sh
+++ scripts/enable-cors.sh
@@ -5,11 +5,22 @@
 #chmod +x enable-cors.sh
 #./enable-cors.sh
 
+function get_repo(){
+	if [ -f /etc/centos-release ]; then
+		REPO='rpm'
+ 
+	elif [ -f /etc/debian_version ]; then
+		REPO='apt'
+fi
+}
+
+
 function enable_cors(){
-
-
-sed -i.save $'/<\/web-app>/{e cat /usr/share/webmin/geohelm/scripts/cors.txt\n}' $CATALINA_HOME/conf/web.xml
-
+	if [ "${REPO}" == 'apt' ]; then
+		sed -i.save $'/<\/web-app>/{e cat /usr/share/webmin/geohelm/scripts/cors.txt\n}' $CATALINA_HOME/conf/web.xml
+	elif [ "${REPO}" == 'rpm' ]; then
+		sed -i.save $'/<\/web-app>/{e cat /usr/libexec/webmin/geohelm/scripts/cors.txt\n}' $CATALINA_HOME/conf/web.xml
+	fi
 }
 
 enable_cors;
