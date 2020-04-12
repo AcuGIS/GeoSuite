206434a33326f94e42cf0f5acfa883ff8bf5ffd6
David Ghedini <david@davidghedini.com>
2020-04-12 14:35:55 +0300

    Update csrf-whitelist.sh

--- scripts/csrf-whitelist.sh
+++ scripts/csrf-whitelist.sh
@@ -14,13 +14,29 @@ function get_repo(){
 fi
 }
 
+# Prompt the user for domain
+echo "Domain to whitelist: "
+
+# Read the input to a variable
+read DOMAIN
+
+
+function update_domain(){
+	if [ "${REPO}" == 'apt' ]; then
+	        sed -i 's/yourdomain.com/$DOMAIN/g' /usr/share/webmin/geohelm/scripts/csrf-whitelist.txt
+	elif [ "${REPO}" == 'rpm' ]; then
+		sed -i 's/yourdomain.com/$DOMAIN/g' /usr/libexec/webmin/geohelm/scripts/csrf-whitelist.txt
+	fi
+}
+
 
 function enable_csrf(){
 	if [ "${REPO}" == 'apt' ]; then
-		sed -i.save $'/<\/web-app>/{e cat /usr/share/webmin/geohelm/scripts/csrf-whitelist.txt\n}' $CATALINA_HOME/webapps/geoserver/WEB-INF/web.xml
+	        sed -i.save $'/<\/web-app>/{e cat /usr/share/webmin/geohelm/scripts/csrf-whitelist.txt\n}' $CATALINA_HOME/webapps/geoserver/WEB-INF/web.xml
 	elif [ "${REPO}" == 'rpm' ]; then
 		sed -i.save $'/<\/web-app>/{e cat /usr/libexec/webmin/geohelm/scripts/csrf-whitelist.txt\n}' $CATALINA_HOME/webapps/geoserver/WEB-INF/web.xml
 	fi
 }
 
+update_domain;
 enable_csrf;
